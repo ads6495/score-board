e1a9c30cdae0a3781bf8acea076d55ba321c9a10
ads6495 <astrange55@icloud.com>
2019-08-06 13:03:03 -0400

    removed unused variables, dry

--- public/main.js
+++ public/main.js
@@ -18,72 +18,67 @@ let team2Name = document.querySelector('.team-2-name')
 // update button selector
 const updateTeam1Name = document.querySelector('.update-team-1-name')
 const updateTeam2Name = document.querySelector('.update-team-2-name')
-// add 1 button 
+// add 1 button
 const team1Add = document.querySelector('.team-1-add-1-button')
 const team2Add = document.querySelector('.team-2-add-1-button')
-//subtract one 
-const team1subtract = document.querySelector('.team-1-substract-1-button')
-const team2subtract = document.querySelector('.team-2-substract-1-button')
+//subtract one
+
 // select team 1 score
 let team1Score = document.querySelector('.team-1-score')
 let team12Score = document.querySelector('.team-2-score')
 
-
 const add1ToTeam1Score = () => {
   // get the current value
-  const currentScore = document.querySelector(".team-1-score").textContent
+  const currentScore = document.querySelector('.team-1-score').textContent
   // add 1
   const newScore = parseInt(currentScore) + 1
   // update the html
-  document.querySelector(".team-1-score").textContent = newScore
+  document.querySelector('.team-1-score').textContent = newScore
 }
 
 const add1toTeam2score = () => {
-  const currentScore = document.querySelector(".team-2-score").textContent
+  const currentScore = document.querySelector('.team-2-score').textContent
   const newScore = parseInt(currentScore) + 1
-  document.querySelector(".team-2-score").textContent = newScore
+  document.querySelector('.team-2-score').textContent = newScore
 }
 
-
 const subtract1ToTeam1Score = () => {
-  const currentScore = document.querySelector(".team-1-score").textContent
+  const currentScore = document.querySelector('.team-1-score').textContent
   const newScore = parseInt(currentScore) - 1
-  document.querySelector(".team-1-score").textContent = newScore
+  document.querySelector('.team-1-score').textContent = newScore
 }
 
 const subtract1ToTeam2Score = () => {
-  const currentScore = document.querySelector(".team-2-score").textContent
+  const currentScore = document.querySelector('.team-2-score').textContent
   const newScore = parseInt(currentScore) - 1
-  document.querySelector(".team-2-score").textContent = newScore
+  document.querySelector('.team-2-score').textContent = newScore
 }
 
 const newTeam1Name = () => {
   team1Name.innerHTML = team1Input.value
 }
-updateTeam1Name.addEventListener("click", newTeam1Name)
+updateTeam1Name.addEventListener('click', newTeam1Name)
 
 const newTeam2Name = () => {
   team2Name.innerHTML = team2Input.value
 }
-updateTeam2Name.addEventListener("click", newTeam2Name)
+updateTeam2Name.addEventListener('click', newTeam2Name)
 
 // eventlisteners
 document
-  .querySelector(".team-1-add-1-button")
+  .querySelector('.team-1-add-1-button')
   .addEventListener('click', add1ToTeam1Score)
 
 document
-  .querySelector(".team-2-add-1-button")
+  .querySelector('.team-2-add-1-button')
   .addEventListener('click', add1toTeam2score)
 
 document
-  .querySelector(".team-1-subtract-1-button")
+  .querySelector('.team-1-subtract-1-button')
   .addEventListener('click', subtract1ToTeam1Score)
 
 document
-  .querySelector(".team-2-subtract-1-button")
+  .querySelector('.team-2-subtract-1-button')
   .addEventListener('click', subtract1ToTeam2Score)
-
-
 
 document.addEventListener('DOMContentLoaded', main)
